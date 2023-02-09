f96785db20384bc1a5eb37e5c41618889eee2b14
sentil <sentilkumaran06@gmail.com>
2023-02-10 00:40:20 +0530

    paiddenominations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,7 +2,7 @@ class Bill < ApplicationRecord
   belongs_to :shop
   has_many :carts,dependent: :destroy
   accepts_nested_attributes_for :carts,allow_destroy: true
-  has_many :amounts
+  has_many :amounts,dependent: :destroy
   def amounts_attributes=(amounts_attributes)
     amounts_attributes.each do|i,amount_attribute|
       self.amounts.build(amount_attribute)
